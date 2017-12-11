36cab4ed1946f5acd23073d27f3d68f900c3303c
Ana Júlia Borges <aninhadas@gmail.com>
2017-12-11 18:31:42 -0200

    Av2_Lessa
    Agr foi

---
+++ Av2_Lessa/Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class Bullet : MonoBehaviour
+{
+    public Rigidbody2D bulletRigidbody;
+
+    public Vector2 impulse;
+
+	public AudioClip Explosion;
+
+    public GameObject ExplosionS;
+    private void Start()
+    {
+        bulletRigidbody.AddForce(impulse, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        Camera.main.GetComponent<CameraShake>().Shake();
+        Destroy(gameObject);
+		AudioSource.PlayClipAtPoint (Explosion, transform.position);
+        Instantiate(ExplosionS, transform.position, Quaternion.identity);
+        
+    }
+}
---
+++ Av2_Lessa/Assets/Scripts/Cannon.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public sealed class Cannon : MonoBehaviour
+{
+    public Bullet bulletPrefab;
+    public float instantiateOffset = 10.0f;
+    public float shootImpulse = 30.0f;
+
+    public Vector3 startingDirection = Vector3.left;
+    public int balas = 5;
+
+    public AudioClip atirando;
+    public TextMesh municao;
+    public Text aperte;
+
+    private void Start()
+    {
+        aperte.enabled = false;
+    }
+
+
+    private void Update()
+    {
+        
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 mouseDirection = Input.mousePosition - screenPosition;
+        mouseDirection.z = 0.0f;
+
+        var rotation = Quaternion.FromToRotation(startingDirection, mouseDirection);
+        transform.localRotation = rotation;
+
+        if (Input.GetMouseButtonDown(0) && balas > 0)
+        {
+            balas -= 1;
+            Vector2 direction = ((Vector2)mouseDirection).normalized;
+            Vector3 position = transform.position + (Vector3)direction * instantiateOffset;
+            AudioSource.PlayClipAtPoint(atirando, transform.position);
+            var bullet = Instantiate(bulletPrefab.gameObject, position, Quaternion.identity).GetComponent<Bullet>();
+            bullet.impulse = direction * shootImpulse;
+        }
+
+        municao.text = balas.ToString();
+
+        if(balas == 0)
+        {
+            aperte.enabled = true;
+        }
+
+        if (Input.GetMouseButtonDown(1) && balas < 5)
+        {
+            balas = 5;
+            aperte.enabled = false;
+        }
+
+           
+        }
+    }
+
